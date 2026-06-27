06beed5bd7c0454f4b875e8afea8be979fd112d9
SimmoM8 <benjaminsimmons@outlook.com.au>
2026-06-27 21:38:44 +0200

    deleted tests

--- tests/mocks/expo-file-system-legacy.cjs
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  documentDirectory: 'file:///tmp/',
-  cacheDirectory: 'file:///tmp/',
-  EncodingType: { UTF8: 'utf8' },
-  writeAsStringAsync: async () => {},
-  getInfoAsync: async () => ({ exists: false, size: 0 }),
-};
