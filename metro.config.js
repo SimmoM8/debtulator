900c3bd8e7de976dda5bc6c97e07f9eeb25f3df3
SimmoM8 <benjaminsimmons@outlook.com.au>
2026-07-19 01:21:35 +0200

    fix: bundle Expo SQLite wasm on web

--- metro.config.js
+++ metro.config.js
@@ -5,4 +5,7 @@ const config = getDefaultConfig(__dirname);
 // Work around local Watchman stalls that can block bundle generation.
 config.resolver.useWatchman = false;
 
+// Expo SQLite's web worker imports its WebAssembly binary as an asset.
+config.resolver.assetExts.push('wasm');
+
 module.exports = config;
