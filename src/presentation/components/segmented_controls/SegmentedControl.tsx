e3dd60ce92c923e20127e1b60676f5551edfc019
SimmoM8 <157427658+SimmoM8@users.noreply.github.com>
2026-08-23 20:17:51 +0200

    refactor: remove old segmented control path

--- src/presentation/components/segmented_controls/SegmentedControl.tsx
+++
@@ -1,7 +0,0 @@
-import type { SegmentedControlProps } from "./SegmentedControl.types";
-
-export function SegmentedControl<T extends string>(
-  _props: SegmentedControlProps<T>,
-) {
-  return null;
-}
