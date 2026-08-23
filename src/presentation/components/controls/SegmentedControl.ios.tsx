d601adee7e90d5fe55b2fafb39f19b389e175a0f
SimmoM8 <157427658+SimmoM8@users.noreply.github.com>
2026-08-23 20:15:20 +0200

    refactor: move iOS SegmentedControl into controls

---
+++ src/presentation/components/controls/SegmentedControl.ios.tsx
@@ -0,0 +1,47 @@
+import { Host, Picker, Text } from "@expo/ui/swift-ui";
+import { pickerStyle, tag } from "@expo/ui/swift-ui/modifiers";
+import { StyleSheet, View } from "react-native";
+
+import type { SegmentedControlProps } from "./SegmentedControl.types";
+
+export function SegmentedControl<T extends string>({
+  value,
+  options,
+  onChange,
+  accessibilityLabel = "Segmented control",
+  colorScheme,
+}: SegmentedControlProps<T>) {
+  return (
+    <View style={styles.container}>
+      <Host colorScheme={colorScheme} style={styles.host}>
+        <Picker
+          label={accessibilityLabel}
+          selection={value}
+          onSelectionChange={(selection) => {
+            onChange(selection as T);
+          }}
+          modifiers={[pickerStyle("segmented")]}
+        >
+          {options.map((option) => (
+            <Text key={option.value} modifiers={[tag(option.value)]}>
+              {option.label}
+            </Text>
+          ))}
+        </Picker>
+      </Host>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    width: "100%",
+    height: 36,
+    overflow: "hidden",
+  },
+
+  host: {
+    width: "100%",
+    height: 36,
+  },
+});
