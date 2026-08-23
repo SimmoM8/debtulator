a1c8e5749886676fb9950c622b32fdb158e944e3
SimmoM8 <157427658+SimmoM8@users.noreply.github.com>
2026-08-23 20:15:59 +0200

    refactor: use presentation layout components

--- app/(tabs)/debts/_layout.tsx
+++ app/(tabs)/debts/_layout.tsx
@@ -1,4 +1,4 @@
-import { NavHeader } from "@/src/components/layout";
+import { NavHeader } from "@/src/presentation/components/layout";
 
 export default function DebtsLayout() {
   return <NavHeader />;
