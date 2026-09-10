b3b222c22f93c8e896397310d9a76d85858d5a57
SimmoM8 <benjaminsimmons@outlook.com.au>
2026-09-10 23:20:19 +0200

    feat: set up test environment with PostgreSQL container and user schema

--- backend/src/test/java/com/debtulator/backend/BackendApplicationTests.java
+++ backend/src/test/java/com/debtulator/backend/BackendApplicationTests.java
@@ -1,13 +1,17 @@
 package com.debtulator.backend;
 
+import com.debtulator.backend.config.TestDatabaseConfiguration;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ActiveProfiles;
 
+@Import(TestDatabaseConfiguration.class)
+@ActiveProfiles("test")
 @SpringBootTest
 class BackendApplicationTests {
 
     @Test
     void contextLoads() {
     }
-
 }
